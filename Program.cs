197e8152daf49f7cc87e10f315c99a56b6ac99f7
CHAN Suvannet <chansuvannet999@gmail.com>
2025-06-03 21:13:40 +0700

    get all coin data for real time update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
 using CoinXplorer_API.Services;
+using CoinXplorer_API.Hubs;
+using AppRoute.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add controllers service to enable API controllers
+// Register services
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// HttpClient can be singleton, no problem
 builder.Services.AddHttpClient();
+
+// Register CoinExternalService as scoped (good practice if it uses HttpClient)
 builder.Services.AddScoped<CoinExternalService>();
+
+// SignalR
+builder.Services.AddSignalR();
+
+// Register the background service (singleton by default)
+builder.Services.AddHostedService<CoinBroadcastService>();
 
 var app = builder.Build();
 
@@ -20,7 +31,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Enable routing for API controllers
 app.MapControllers();
+app.MapAppRoutes();
+
+app.MapHub<CoinHub>("/coinhub");
 
 app.Run();
---
+++ src/app/Routes/Route.cs
@@ -0,0 +1,13 @@
+using CoinXplorer_API.Routes;
+
+namespace AppRoute.Routes
+{
+    public static class AppRoutes
+    {
+        public static void MapAppRoutes(this IEndpointRouteBuilder app)
+        {
+            var apiGroup = app.MapGroup("/api");
+            apiGroup.MapCoinRoutes();
+        }
+    }
+}
