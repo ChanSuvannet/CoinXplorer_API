12b24f4f6fe65a15cebf1a14e50c6997ea13d90a
CHAN Suvannet <chansuvannet999@gmail.com>
2025-06-03 23:08:04 +0700

    update file

--- src/app/resources/Services/CoinHub.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.SignalR;
-
-namespace CoinXplorer_API.Hubs
-{
-    public class CoinHub : Hub
-    {
-        // Optional: add hub methods here for client calls
-    }
-}
--- src/app/resources/services/CoinHub.cs
+++ src/app/resources/services/CoinHub.cs
@@ -5,5 +5,9 @@ namespace CoinXplorer_API.Hubs
     public class CoinHub : Hub
     {
         // Optional: add hub methods here for client calls
+        public async Task SendCoinUpdate(string message)
+        {
+            await Clients.All.SendAsync("ReceiveCoinUpdate", message);
+        }
     }
 }
